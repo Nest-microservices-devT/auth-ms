e33824630f0ff0adb8ec81e7b0e2d2f2d4026a31
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2024-04-07 11:23:32 -0500

    First commit

---
+++ src/config/envs.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+import { EnvVars } from 'src/interfaces';
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS.split(','),
+});
+
+if (error) throw new Error(`Config validation error: ${error.message}`);
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  nats_servers: envVars.NATS_SERVERS,
+};
